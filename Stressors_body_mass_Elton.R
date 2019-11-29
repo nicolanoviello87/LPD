aa3b7f20a7b95c478c40f26c927db1f234c359a6
nicolanoviello87 <56927871+nicolanoviello87@users.noreply.github.com>
2019-11-29 17:11:14 +0000

    Update Stressors_body_mass_Elton.R

--- Stressors_body_mass_Elton.R
+++ Stressors_body_mass_Elton.R
@@ -16,15 +16,13 @@ library(readr)
 library(writexl)
 
 #read in datasets
-lpi <- read_xlsx("C:/PhD/LPI_Data/LPI.xlsx")
+lpi <- read_xlsx("C:/PhD/LPI_Data/LPI_Threats.xlsx")
 elton <- read_xlsx("C:/PhD/Supplementary_Datasets/Elton_Traits/Elton_Traits.xlsx")
 
 #correct formatting
 lpi = as.matrix(lpi)
 lpi[lpi=="NULL"] <- NA
 lpi = as.data.frame(lpi)
-lpi <- lpi[lpi$Threat_status !="Unknown (no information)",]
-lpi <- lpi[lpi$Threat_status !="Unknown (large data set)",]
 
 #rename column
 colnames(elton)[8] <- "Binomial"
@@ -39,7 +37,7 @@ elton$Binomial <- sub(" ", "_", elton$Binomial)
 elt <- merge(lpi, elton, by="Binomial")
 
 #reduce data frame to only those traits deemed useful
-eltbs <- elt[c(1, 2, 15, 23, 30, 35, 37,  39, 42, 47, 52, 144, 186, 221)]
+eltbs <- elt[c(1, 2, 15, 23, 30, 35, 47, 52, 144, 181, 186, 221)]
 
 #homogenise body mass column names
 colnames(eltbs)[12] <- "bs"
@@ -49,54 +47,46 @@ eltbs <- eltbs %>% distinct(ID, .keep_all = TRUE)
 
 #remove rows where body mass is less than 0
 eltbs$bs = as.numeric(eltbs$bs)
+eltbs$bs <- log(eltbs$bs)
 eltbs <- eltbs[eltbs$bs >= 0, ]
 
-#remove rows with NAs
-eltbs <- eltbs[complete.cases(eltbs), ]
 
 #number of species represented (758) and total number of time series (2371)
 species <- length(unique(eltbs[,"Binomial"]))
 totalTS <- sum(eltbs$Class == "Aves")
 
 #plot all mammals by system in facets
-ggplot(eltbs, aes(bs, no_stress)) +
+ggplot(eltbs, aes(bs, no_stress, colour = System)) +
   geom_point(stat = "identity") +
-  stat_smooth(method = "glm") +
-  labs(x = "Body Mass (g)", y = "Average Number of Threats", size = 20) +
-  ggtitle("Body Size vs Threats for Birds by System") +
-  facet_wrap(~System, scales = "free")
+  stat_smooth(method = "glm", method.args = list(family = "poisson"), se=FALSE) +
+  labs(x = "Log Body Mass", y = "Average Number of Threats", size = 20) +
+  ggtitle("Bird Body Size vs Threats by System") +ylim(0,3)+
+  scale_colour_discrete(na.translate = F)
 
-ggplot(eltbs, aes(bs, no_stress)) +
+ggplot(eltbs, aes(bs, no_stress, colour = Region)) +
   geom_point(stat = "identity") +
-  stat_smooth(method = "glm") +
-  labs(x = "Body Mass (g)", y = "Average Number of Threats", size = 20) +
-  ggtitle("Body Size vs Threats for Birds by GROMS Category") +
-  facet_wrap(~GROMS_category, scales = "free")
+  stat_smooth(method = "glm", method.args = list(family = "poisson"), se=FALSE) +
+  labs(x = "Log Body Mass", y = "Number of Threats", size = 20) +
+  ggtitle("Bird Body Size vs Threats by Region") +ylim(0,3)+
+  scale_colour_discrete(na.translate = F)
 
-ggplot(eltbs, aes(bs, no_stress)) +
+ggplot(eltbs, aes(bs, no_stress, colour = Red_list_category)) +
   geom_point(stat = "identity") +
-  stat_smooth(method = "glm") +
-  labs(x = "Body Mass (g)", y = "Average Number of Threats", size = 20) +
-  ggtitle("Body Size vs Threats for Birds by Red List Category") +
-  facet_wrap(~Red_list_category, scales = "free")
+  stat_smooth(method = "glm", method.args = list(family = "poisson"), se=FALSE) +
+  labs(x = "Log Body Mass", y = "Average Number of Threats", size = 20) +
+  ggtitle("Bird Body Size vs Threats by Red List Status") +ylim(0,3)+
+  scale_colour_discrete(na.translate = F)
 
-ggplot(eltbs, aes(bs, no_stress)) +
+ggplot(eltbs, aes(bs, no_stress, colour = GROMS_category)) +
   geom_point(stat = "identity") +
-  stat_smooth(method = "glm") +
-  labs(x = "Body Mass (g)", y = "Average Number of Threats", size = 20) +
-  ggtitle("Body Size vs Threats for Birds by Terrestrial Biome") +
-  facet_wrap(~T_biome, scales = "free")
+  stat_smooth(method = "glm", method.args = list(family = "poisson"), se=FALSE) +
+  labs(x = "Log Body Mass", y = "Number of Threats", size = 20) +
+  ggtitle("Bird Body Size vs Threats by GROMS Category") +ylim(0,3)+
+  scale_colour_discrete(na.translate = F)
 
-ggplot(eltbs, aes(bs, no_stress)) +
+ggplot(eltbs, aes(bs, no_stress, colour = Trophic_level)) +
   geom_point(stat = "identity") +
-  stat_smooth(method = "glm") +
-  labs(x = "Body Mass (g)", y = "Average Number of Threats", size = 20) +
-  ggtitle("Body Size vs Threats for Birds by Marine Biome") +
-  facet_wrap(~M_biome, scales = "free")
-
-ggplot(eltbs, aes(bs, no_stress)) +
-  geom_point(stat = "identity") +
-  stat_smooth(method = "glm") +
-  labs(x = "Body Mass (g)", y = "Average Number of Threats", size = 20) +
-  ggtitle("Body Size vs Threats for Birds by Freshwater Biome") +
-  facet_wrap(~FW_biome, scales = "free")
+  stat_smooth(method = "glm", method.args = list(family = "poisson"), se=FALSE) +
+  labs(x = "Log Body Mass", y = "Number of Threats", size = 20) +
+  ggtitle("Bird Body Size vs Threats by Trophic Level") +ylim(0,3)+
+  scale_colour_discrete(na.translate = F)
