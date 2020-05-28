b651a692995e6282c19f464569411536133015b9
nicolanoviello87 <56927871+nicolanoviello87@users.noreply.github.com>
2020-05-28 14:40:21 +0100

    Delete Wide_to_Long.R

--- Wide_to_Long.R
+++
@@ -1,8 +0,0 @@
-#read in original csv file
-LPI <- read_csv("LPI/lpi_original.csv")
-
-#simple wide to long data format conversion; new dataframe renamed
-LPI_Long <- gather(LPI, Year, Pop, 65:133)
-
-#export new csv file at long format
-write.csv(LPI2, file = "LPI_Long.csv") #data nissing with this function
